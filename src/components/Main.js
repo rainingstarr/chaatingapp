179baf0a8e9f85c09a4a8f11c3d4ceea0fdf559d
rainingstarr <122976349+rainingstarr@users.noreply.github.com>
2023-04-14 17:39:05 +0900

    이제 말풍선을 만듭시다

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,36 @@
+import { useEffect, useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import styles from '../css/Main.module.css';
-import data from '../data.js';
+import serverData from '../data.js';
 import Message from './Message.js';
 
 function Main() {
+    function sortByTime(data) {
+        const sortedData = data.sort((a, b) => {
+          const timeA = a.time.replace(/\D/g, '');
+          const timeB = b.time.replace(/\D/g, '');
+          return timeB - timeA;
+        });
+        return sortedData;
+    }
+    const [data,setData] = useState(sortByTime(serverData));
+    const [messageId, setMessageId] = useState();
+
+    useEffect(() => {
+        setData(sortByTime(data));
+    },[data])
+
+      
     return(
-        <div className={styles.wrap}>
+        <div className={styles.wrap} style={messageId === undefined ? {width:`600px`}:{width:`1400px`}}>
             <div className={styles.header}>
-                <Link className={styles.small}><img src={process.env.PUBLIC_URL+"img/icon_06.svg"}></img></Link>
-                <Link><img src={process.env.PUBLIC_URL+"img/icon_07.svg"}></img></Link>
-                <Link><img src={process.env.PUBLIC_URL+"img/icon_08.svg"}></img></Link>
-                <Link className={styles.logOut}><img src={process.env.PUBLIC_URL+"img/icon_09.svg"}></img></Link>
+                <Link to="/main" className={styles.small}><img src={process.env.PUBLIC_URL+"img/icon_06.svg"}></img></Link>
+                <Link to="/main/profile"><img src={process.env.PUBLIC_URL+"img/icon_07.svg"}></img></Link>
+                <Link to="/main/option"><img src={process.env.PUBLIC_URL+"img/icon_08.svg"}></img></Link>
+                <Link to="/" className={styles.logOut}><img src={process.env.PUBLIC_URL+"img/icon_09.svg"}></img></Link>
             </div>
             <Routes>
-                <Route path="/" element={<Message data={data}/>}></Route>
+                <Route path="/" element={<Message data={data} messageId={messageId} setMessageId={setMessageId}/>}></Route>
             </Routes>
         </div>
 
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,23 +1,82 @@
-function Message(props) {
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
+function Message(props) {
+    const [alert, setAlert] = useState(false);
+    const [alertImageSrc, setAlertImageSrc] = useState('img/icon_03.svg');
+    function alertChange(){
+        if(alert === false){
+            setAlert(true);
+            setAlertImageSrc('img/icon_04.svg');
+        }else{
+            setAlert(false);
+            setAlertImageSrc('img/icon_03.svg');
+        }
+    }
+    function searchRoom(e){
+        let search = e.target.value;
+        let list = document.querySelectorAll('.chatting');
+        list.forEach((item) => {
+            if(item.querySelector('.chatting_name').innerText.indexOf(search) > -1){
+                item.style.display = 'block';
+            }else{
+                item.style.display = 'none';
+            }
+        })
+    }
+    function changeTime(newDate){
+        const currentDate = newDate;
+        const formattedDate = `${currentDate.getFullYear().toString().slice(-2).padStart(2,'0')}-${(currentDate.getMonth()+1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')} ${currentDate.getHours().toString().padStart(2, '0')}:${currentDate.getMinutes().toString().padStart(2, '0')}`;
+        return(formattedDate);
+    }
     return(
-        <div className="chattingRoom">
-            <input className="search" placeholder="검색"></input><img className="search_img" src={process.env.PUBLIC_URL+"img/icon_02.svg"}></img>
-            <div className="chattingList">
-                {props.data.map((item, index) => {
-                    return(
-                        <div className="chatting" key={index}>
-                            <div className="profile_con"><img className="profile" src={process.env.PUBLIC_URL+"img/img_profile_"+item.profile+".jpg"}></img></div>
-                            <div className="chatting_info">
-                                <div className="chatting_name">{item.name}</div>
-                                <div className="chatting_message">{item.message}</div>
+        <>
+            <div className="chattingRoom">
+                
+                <label>
+                    <input className="search" placeholder="검색" onChange={searchRoom}></input><img className="search_img" src={process.env.PUBLIC_URL+"img/icon_02.svg"}></img>
+                </label>
+                <div className="chattingList">
+                    {props.data.map((item, index) => {
+                        return(
+                            <div className="chatting" onClick={()=>{props.setMessageId(item.id)}} key={index}>
+                                <div className={`profile_con ${item.status}`} style={{background:`url(${process.env.PUBLIC_URL}img/img_profile_${item.profile}.jpg) no-repeat 50% / cover`}}></div>
+                                <div className="chatting_info">
+                                    <div className="chatting_name">{item.name}</div>
+                                    <div className="chatting_message">{item.message}</div>
+                                </div>
+                                {changeTime(new Date()).split(' ')[0] === item.time.split(' ')[0] ? <div className="chatting_time">{item.time.split(' ')[1]}</div> : <div className="chatting_time">{item.time.split(' ')[0]}</div>}
                             </div>
-                            <div className="chatting_time">{item.time}</div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
+            </div>
+            {props.messageId === undefined ? null: <ChattingScreen data={props.data.find(item => item.id === props.messageId)} alertImageSrc={alertImageSrc} alertChange={alertChange} setMessageId={props.setMessageId}/>}
+        </>
+    )
+}
+
+function ChattingScreen (props){
+    function search (){};
+    return(
+        <div className="chattingScreen">
+            <div className="header">
+                <label>
+                    <input className="search" placeholder="내용검색" onChange={search}></input><img className="search_img" src={process.env.PUBLIC_URL+"img/icon_02.svg"}></img>
+                </label>
+                <div className="btn_b btns" onClick={props.alertChange}><img src={process.env.PUBLIC_URL+props.alertImageSrc}></img></div>
+                <div className="btn_b btns close" onClick={()=>{props.setMessageId(undefined)}}></div>
+                <div className="info">
+                    <div className="name">{props.data.name}</div>
+                    <div className="status">{props.data.status}</div>
+                </div>
+            </div>
+            <div className="messageScreen">
+                <div className="left"><div className="message">밥먹으러 가자</div><div className="time">22:03</div></div>
+                <div className="right"><div className="time">22:05</div><div className="message">시러!</div></div>
             </div>
         </div>
     )
 }
+
 export default Message;
